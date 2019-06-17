138bb06def5b740a22e5686dc4bea1596c057cd9
Ignacio Lorenzo <49351085+Njdream@users.noreply.github.com>
2019-06-16 20:09:43 -0600

    Add files via upload

--- Proyecto 3.py
+++ Proyecto 3.py
@@ -5,12 +5,12 @@ from data_base import*
 #Funcion para elegir a la escuderia que se quiere entrar
 def cambiar():
     e=Tk()
-    e.title("Escudeía")
+    e.title("Escudería")
     e.geometry("500x500")
     e.resizable(False,False)
     e.config(bd=15)
     e.config(relief="sunken")
-    ESC=Label(e,text="Elija una escudeía").place(x=20,y=20)
+    ESC=Label(e,text="Elija una escudería").place(x=20,y=20)
     esc=ttk.Combobox(e)
     esc['values']=("NjSarc","Mercedes","Ferrari","McLaren","Red Bull")
     esc.current(0)
@@ -23,34 +23,39 @@ def cambiar():
            logo=PhotoImage(file="imagenes\mercedes.png")
            patro=PhotoImage(file="imagenes\Imperial.png")
            Esc="Escudería: Mercedes"
-           Ven1(logo,patro,Esc,m)
+           geo="Ubicacion geografica: Alemania"
+           Ven1(logo,patro,Esc,m,geo)
         elif(m=="Ferrari"):
             logo=PhotoImage(file="imagenes\errari.png")
             logo=logo.subsample(4,4)
             patro=PhotoImage(file="imagenes\pilsen.png")
             Esc="Escudería: Ferrari"
-            Ven1(logo,patro,Esc,m)
+            geo="Ubicacion geografica: Italia"
+            Ven1(logo,patro,Esc,m,geo)
         elif(m=="McLaren"):
             logo=PhotoImage(file="imagenes\mclaren.png")
             logo=logo.zoom(2,2)
             patro=PhotoImage(file="imagenes\heineken.png")
             Esc="Escudería: McLaren"
-            Ven1(logo,patro,Esc,m)
+            geo="Ubicacion geografica: Reino Unido"
+            Ven1(logo,patro,Esc,m,geo)
         elif(m=="NjSarc"):
             logo=PhotoImage(file="imagenes\jSarc.png")
             logo=logo.subsample(1,1)
             patro=PhotoImage(file="imagenes\Bananacode.png")
             patro=patro.subsample(2,1)
             Esc="Escudería: NjSarc"
-            Ven1(logo,patro,Esc,m)
+            geo="Ubicacion geografica: Costa Rica"
+            Ven1(logo,patro,Esc,m,geo)
         else:
             logo=PhotoImage(file="imagenes\edbull.png")
             patro=PhotoImage(file="imagenes\pirelli.png")
             patro=patro.subsample(6,6)
             Esc="Escudería: Red Bull"
-            Ven1(logo,patro,Esc,m)
+            geo="Ubicacion geografica: Austria"
+            Ven1(logo,patro,Esc,m,geo)
     ele=Button(e,text="Elegir",command=mandar).place(x=20,y=80)
-def Ven1(logo,patro,Esc,m):
+def Ven1(logo,patro,Esc,m,geo):
     IGE=escuderia_info(m)
     IGE= "Indice de ganador:  "+str(IGE)
     ven1=Toplevel()
@@ -61,6 +66,7 @@ def Ven1(logo,patro,Esc,m):
     ven1.config(relief="sunken")
     esc=Label(ven1,text=Esc).place(x=350,y=0)
     temp=Label(ven1,text="Temporada: 2019").place(x=0,y=25)
+    geografia=Label(ven1,text=geo).place(x=0,y=50)
     est=Label(ven1,text="Estado del vehiculo: ").place(x=300,y=25)
     ind=Label(ven1,text=IGE).place(x=525,y=25)
     log=Label(ven1,text="Logo").place(x=75,y=100)
@@ -69,6 +75,37 @@ def Ven1(logo,patro,Esc,m):
     patro=patro.subsample(2,2)
     patrocinadores=Label(ven1,image=patro).place(x=450,y=150)
     #Informacion de los desarrolladores
+    def nuevo():
+            n=Tk()
+            n.title("Ingrese sus Datos")
+            n.geometry("500x500")
+            n.config(bd=15)
+            n.config(relief="sunken")
+            nombre=Label(n,text="Nombre").place(x=20,y=20)
+            nom=Entry(n)
+            nom.place(x=20,y=45)
+            pais=Label(n,text="País").place(x=20,y=70)
+            pa=Entry(n)
+            pa.place(x=20,y=95)
+            escuderia=Label(n,text="Escudería").place(x=20,y=120)
+            esc=ttk.Combobox(n)
+            esc['values']=("NjSarc","Mercedes","Ferrari","McLaren","Red Bull")
+            esc.current(0)
+            esc.config(state="readonly")
+            esc.place(x=20,y=145)
+            temporada=Label(n,text="Temporada").place(x=20,y=170)
+            temp=Entry(n)
+            temp.place(x=20,y=195)
+            def Agregar():
+                n.withdraw()
+                N=nom.get()
+                P=pa.get()
+                E=esc.get()
+                T=temp.get()
+                x=[N,P,E,T]
+                nuevo_piloto(N,P,E,int(T))
+                            
+            agregar=Button(n,text="Agragar Piloto",command=Agregar).place(x=20,y=250)
     def about():
         about=Tk()
         about.title("About")
@@ -165,41 +202,147 @@ def Ven1(logo,patro,Esc,m):
                 cell.insert(0,a[i][o])
                 i=i
                 o+=1
-        #Funcion para pedir los datos e  inscribir un nuevo piloto
+         
+        #Funcion para pedir los datos y agregar puntuacion
+        def editar_piloto():
+                pilotos=pilotos_all()
+                edit=Tk()
+                edit.title("Modificacion de piloto")
+                edit.geometry("300x300")
+                edit.config(bd=15)
+                edit.config(relief="sunken")
+                text=Label(edit,text="Eliga el piloto que quiere editar:").place(x=10,y=40)
+                piloto=ttk.Combobox(edit)
+                piloto['values']= pilotos
+                piloto.current(0)
+                piloto.config(state="readonly")
+                piloto.place(x=10,y=65)
+                def cambiar_pais():
+                    cambp=Tk()
+                    cambp.geometry("350x350")
+                    cambp.title("Cambiando pais")
+                    cambp.config(bd=15)
+                    cambp.config(relief="sunken")
+                    text=Label(cambp,text="Nuevo pais del piloto:").place(x=10,y=60)
+                    pa=Entry(cambp)
+                    pa.place(x=10,y=90)
+                    def cambio_pais():
+                        name=piloto.get()
+                        pais=pa.get()
+                        change_pais(name,pais)
+                        cambp.destroy()
+                        edit.destroy()
+                    def volver():
+                        cambp.destroy()
+                    
+                    elegirpais=Button(cambp,text="Cambiar",command=cambio_pais).place(x=10,y=145)
+                    cerrarpais=Button(cambp,text="Volver",command=volver).place(x=80,y=145)
+                def cambiar_escuderia():
+                    cambe=Tk()
+                    cambe.geometry("350x350")
+                    cambe.title("Cambiando escuderia")
+                    cambe.config(bd=15)
+                    cambe.config(relief="sunken")
+                    text=Label(cambe,text="Nueva escuderia del piloto:").place(x=10,y=60)
+                    esc=ttk.Combobox(cambe)
+                    esc['values']=("NjSarc","Mercedes","Ferrari","McLaren","Red Bull")
+                    esc.current(0)
+                    esc.config(state="readonly")
+                    esc.place(x=10,y=90)
+                    def cambio_escuderia():
+                        name=piloto.get()
+                        escuderia= esc.get()
+                        change_escuderia(name,escuderia)
+                        cambe.destroy()
+                        edit.destroy()
+                    def volver():
+                        cambe.destroy()
+                    
+                    elegirpais=Button(cambe,text="Cambiar",command=cambio_escuderia).place(x=10,y=145)
+                    cerrarpais=Button(cambe,text="Volver",command=volver).place(x=80,y=145)
+                def cambiar_temporada():
+                    cambt=Tk()
+                    cambt.geometry("350x350")
+                    cambt.title("Cambiando temporada")
+                    cambt.config(bd=15)
+                    cambt.config(relief="sunken")
+                    text=Label(cambt,text="Nueva temporada del piloto:").place(x=10,y=60)
+                    temporada=Entry(cambt)
+                    temporada.place(x=10,y=90)
+                    def cambio_temporada():
+                        name=piloto.get()
+                        temp=temporada.get()
+                        change_temporada(name,temp)
+                        cambt.destroy()
+                        edit.destroy()
+                    def volver():
+                        cambt.destroy()
+                    
+                    elegirpais=Button(cambt,text="Cambiar",command=cambio_temporada).place(x=10,y=145)
+                    cerrarpais=Button(cambt,text="Volver",command=volver).place(x=80,y=145)
+                def eliminar_piloto():
+                    cambpi=Tk()
+                    cambpi.geometry("405x350")
+                    cambpi.title("Eliminar piloto")
+                    cambpi.config(bd=15)
+                    cambpi.config(relief="sunken")
+                    text=Label(cambpi,text="El piloto se eliminara para siempre").place(x=10,y=60)
+                    text2=Label(cambpi,text="Presione el boton eliminar si esta seguro, si no lo esta presione volver").place(x=10,y=90)
+                    def eliminar_pilotoo():
+                        name=piloto.get()
+                        eliminaar_piloto(name)
+                        cambpi.destroy()
+                        edit.destroy()
+                    def volver():
+                        cambpi.destroy()
+                    
+                    elegirpais=Button(cambpi,text="Eliminar",command=eliminar_pilotoo).place(x=10,y=145)
+                    cerrarpais=Button(cambpi,text="Volver",command=volver).place(x=80,y=145)
+
+
+
+                changepais=Button(edit,text="Cambiar País",command=cambiar_pais).place(x=30,y=100)
+                changeescuderia=Button(edit,text="Cambiar Escuderia",command=cambiar_escuderia).place(x=30,y=130)
+                changetemporada=Button(edit,text="Cambiar Temporada",command=cambiar_temporada).place(x=30,y=160)
+                eliminarpiloto=Button(edit,text="Eliminar piloto",command=eliminar_piloto).place(x=30,y=190)
+
         def nuevo():
             n=Tk()
             n.title("Ingrese sus Datos")
             n.geometry("500x500")
             n.config(bd=15)
             n.config(relief="sunken")
-            nombre=Label(n,text="Nombre").place(x=20,y=20)
+            nombre=Label(n,text="Nombre del piloto:").place(x=20,y=20)
             nom=Entry(n)
             nom.place(x=20,y=45)
-            pais=Label(n,text="País").place(x=20,y=70)
-            pa=Entry(n)
-            pa.place(x=20,y=95)
-            escuderia=Label(n,text="Escudería").place(x=20,y=120)
-            esc=ttk.Combobox(n)
-            esc['values']=("NjSarc","Mercedes","Ferrari","McLaren","Red Bull")
-            esc.current(0)
-            esc.config(state="readonly")
-            esc.place(x=20,y=145)
-            temporada=Label(n,text="Temporada").place(x=20,y=170)
-            temp=Entry(n)
-            temp.place(x=20,y=195)
+            Victorias=Label(n,text="Victorias del piloto:").place(x=20,y=70)
+            Vic=Entry(n)
+            Vic.place(x=20,y=95)
+            Derrotas=Label(n,text="Derrotas del piloto:").place(x=20,y=120)
+            Der=Entry(n)
+            Der.place(x=20,y=145)
+            Canceladas=Label(n,text="Carreras canceladas del piloto:").place(x=20,y=170)
+            Can=Entry(n)
+            Can.place(x=20,y=195)
+            Podio=Label(n,text="Carreras que estuvo en el podio:").place(x=20,y=220)
+            Pod=Entry(n)
+            Pod.place(x=20,y=245)
             def Agregar():
                 n.withdraw()
                 N=nom.get()
-                P=pa.get()
-                E=esc.get()
-                T=temp.get()
-                x=[N,P,E,T]
+                V=Vic.get()
+                D=Der.get()
+                C=Can.get()
+                P=Pod.get()
+                T=int(V)+int(D)+int(C)+int(P)
                 from data_base import nuevo_piloto
-                nuevo_piloto(N,P,E,int(T))
+                puntos(N,int(V),int(D),int(C),int(P),int(T))
                             
-            agregar=Button(n,text="Agragar Piloto",command=Agregar).place(x=20,y=250)     
-        new=Button(pos,text="Nuevo Piloto",command=nuevo).place(x=10,y=500)
-        inicio=Button(pos,text="Inicio", command=pos.withdraw).place(x=10,y=610)
+            agregar=Button(n,text="Mandar datos",command=Agregar).place(x=20,y=280)
+
+        new=Button(pos,text="Agregar/Actualizar puntos",command=nuevo).place(x=10,y=500)
+        inicio=Button(pos,text="Inicio", command=pos.withdraw).place(x=10,y=400)
+        modificar=Button(pos,text="Modificar piloto",command=editar_piloto).place(x=10,y=450)
     #Funcion para el testr drive(visualizacion de carro y controles)
     def eleccion_piloto():
         eleccion=Tk()
@@ -220,6 +363,9 @@ def Ven1(logo,patro,Esc,m):
             eleccion.destroy()
         Elegir=Button(eleccion,text="Elegir",command=seleccionar_piloto).place(x=20,y=250)              
     def test(name):
+        piloto=elegir_piloto(name)
+        Nombre="Nombre: "+piloto[0]
+        nacionalidad="Nacionalidad: "+piloto[1]
         test=Tk()
         c=Canvas(test,width=800,height=650)
         test.geometry("800x650")
@@ -232,13 +378,13 @@ def Ven1(logo,patro,Esc,m):
         c.create_rectangle(230,390,310,530,fill="#464849")
         c.create_rectangle(490,390,570,530,fill="#464849")
         c.create_rectangle(675,25,790,60,fill="white")
-        c.create_text(720,40,text='''"Nombre"''')
+        c.create_text(720,40,text=Nombre)
         c.create_rectangle(0,0,150,20,fill="white")
         c.create_text(70,10,text='''"Nombre del producto"''')
         c.create_rectangle(0,90,90,110,fill="white")
         c.create_text(40,100,text=m)
-        c.create_rectangle(675,65,790,100,fill="white")
-        c.create_text(730,82,text='''"Nacionalidad"''')
+        c.create_rectangle(642,65,790,100,fill="white")
+        c.create_text(710,82,text=nacionalidad)
         c.create_polygon(400,400,100,650,700,650,fill="red")
         c.create_rectangle(700,250,780,290,fill="black")
         celeb=Button(test,text="Celebración",command=celebra).place(x=705,y=260)
@@ -266,6 +412,7 @@ def Ven1(logo,patro,Esc,m):
     About=Button(ven1,text="About",command=about).place(x=30,y=550)
     Posiciones=Button(ven1,text="Tabla de Posiciones",command=elegir_orden).place(x=100,y=550)
     Test=Button(ven1,text="Test Drive",command=eleccion_piloto).place(x=250,y=550)
+    new=Button(ven1,text="Nuevo Piloto",command=nuevo).place(x=345,y=550)
     ven1.mainloop()
 
     
